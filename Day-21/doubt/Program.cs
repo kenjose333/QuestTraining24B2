b59d2c2673a43df0ece20cbb069fa89f49f0dd1e
kenjose333 <138904009+kenjose333@users.noreply.github.com>
2024-10-29 18:06:13 +0530

    Day-21

---
+++ Day-21/doubt/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doubt
+{   public class Shape
+    {
+        public string Color { get; set; }
+    }
+    public class Triangle : Shape
+    {
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle t1 = new Triangle();
+            t1.
+        }
+    }
+}
---
+++ Day-21/nextProblem/Program.cs
@@ -0,0 +1,32 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day21
+{
+    internal class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+        static void Main(string[] args)
+        {
+            BasicConfigurator.Configure();
+            try
+            {
+                Console.Write("enter num1");
+                int i = int.Parse(Console.ReadLine());
+                Console.Write("enter num2");
+                int j = int.Parse(Console.ReadLine());
+                int sum = i + j;
+                Console.WriteLine(sum);
+            }
+            catch (Exception e)
+            {
+                log.Error(e.StackTrace);
+            }
+        }
+    }
+}
